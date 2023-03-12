33ee2992ea05164454bdd0e77a45dd6a4df82c3f
xethlyx <46338199+xethlyx@users.noreply.github.com>
2023-03-12 15:37:35 -0400

    Update

--- hooks/post-install.sh
+++ hooks/post-install.sh
@@ -8,5 +8,5 @@ if bin/confirm -p "Install vim plug?"; then
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-	vim +PlugInstall +qa!
+	vim --clean '+source etc/.vimrc' +PlugInstall +qa!
 fi
